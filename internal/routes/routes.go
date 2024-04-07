f9652d8f21a7847cea9e6351c6aa21e52bc9606d
Mayer-04 <andres.20.chaves@gmail.com>
2024-04-07 12:51:51 -0500

    add:new routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import "net/http"
+
+func SetupRoutes(server *http.ServeMux) {
+
+	server.HandleFunc("GET /products", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	server.HandleFunc("GET /products/{id}", func(w http.ResponseWriter, r *http.Request) {})
+	server.HandleFunc("POST /products", func(w http.ResponseWriter, r *http.Request) {})
+	server.HandleFunc("PUT /products/{id}", func(w http.ResponseWriter, r *http.Request) {})
+	server.HandleFunc("DELETE /products/{id}", func(w http.ResponseWriter, r *http.Request) {})
+
+}
